f456c4e8edefc4b12d4ae1a90352e2bfce05938f
Sourav Maji <tester@linkedin.com>
2025-01-30 13:59:10 -0800

    test PR

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceController.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceController.java
@@ -364,6 +364,8 @@ public class VeniceController {
     adminServer.start();
     if (sslEnabled) {
       secureAdminServer.start();
+    } else {
+      secureAdminServer.close();
     }
     topicCleanupService.start();
     storeBackupVersionCleanupService.ifPresent(AbstractVeniceService::start);
